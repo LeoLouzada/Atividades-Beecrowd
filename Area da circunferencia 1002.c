5f5e5f91c63bb3e0a09a645ef3797514b727de32
Leonardo Louzada <leonardolouzadam@gmail.com>
2023-08-25 16:17:25 -0300

    Primeiro commit das atividades Beecrowd

---
+++ Area da circunferencia 1002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+   float R, A;
+   //float pi = 3.14159;
+
+   // DIGITE O VALOR DO RAIO
+   scanf("%f", &R);
+
+   A = 3.14159 * (R * R);
+
+   printf("A=%0.4f\n", A);
+
+    return 0;
+}
---
+++ Idade em dias 1020.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    scanf("%d", &i);
+
+    printf("%d ano(s)\n", i / 365);
+
+    printf("%d mes(es)\n", (i % 365) / 30);
+
+
+    printf("%d dia(s)\n", (i % 365) % 30);
+
+    return 0;
+}
